944db4f1005aff32a35f24fc94d1c80b2eb42775
sanjay <sanjay139747@gmail.com>
2023-03-26 15:10:33 +0530

    for deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 mongoose.set('strictQuery',false);
-mongoose.connect('mongodb://localhost/IssueTracker')
+// mongoose.connect('mongodb://localhost/IssueTracker')
+// .then(
+// () => {console.log("Database connected successfully")},
+// (err) => {console.log("error occuring while database connecting",err)}
+// );
+
+
+mongoose.connect(process.env.MongodbUrl)
 .then(
 () => {console.log("Database connected successfully")},
 (err) => {console.log("error occuring while database connecting",err)}
